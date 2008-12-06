6fda128a4d6d3089529687246b2e570383875dd4
Christian Jaeger <chrjae@gmail.com>
2008-12-06 19:43:07 +0100

    Don't say to expect hashes if only expecting key/value pairs

--- Mail/GnuPG.pm
+++ Mail/GnuPG.pm
@@ -58,7 +58,7 @@ sub Warn_check_class ( $ $ ) {
   Create a new Mail::GnuPG instance.
 
  Arguments:
-   Paramhash...
+   Parameter key/value pairs:
 
    key    => gpg key id
    keydir => gpg configuration/key directory
